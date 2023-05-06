27f6cae1f1a7237b431e938b71cc368598c23a78
adriancgarcia <adriancgarcia@gmail.com>
2023-05-06 01:19:55 -0500

    Wild Wild Life answered

--- answers.js
+++ answers.js
@@ -17,45 +17,66 @@
 // Fizz Buzz
 ////////////////////////////////
 // 1. Write a javascript application that logs all numbers from 1 - 100.
-    for (let i=1; i<=100; i++){
-        console.log(i)
-    }
+    // for (let i=1; i<=100; i++){
+    //     console.log(i)
+    // }
 
 // 2.If a number is divisible by 3 log "Fizz" instead of the number.
-    for (let i=0; i<=30; i++){    
-    if (i % 3 === 0){
-        console.log ("fizz") 
-        }else {
-            console.log(i)
-    }
-}
+//     for (let i=0; i<=30; i++){    
+//     if (i % 3 === 0){
+//         console.log ("fizz") 
+//         }else {
+//             console.log(i)
+//     }
+// }
     
 // 3. If a number is divisible by 5 log "Buzz" instead of the number.
-for (let i=0; i<=45; i ++) {
-    if (i % 5 === 0){
-        console.log ("Buzz")
-    } else {
-        console.log(i)
-    }
-}
+// for (let i=0; i<=45; i ++) {
+//     if (i % 5 === 0){
+//         console.log ("Buzz")
+//     } else {
+//         console.log(i)
+//     }
+// }
 
 // 4. If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
 
-for (let i=0; i <= 45; i ++) {
-    if (i % 3 === 0){
-        console.log("fizzbuzz")
-    } else if (i % 5 === 0) {
-        console.log ("fizzbuzz")
-    } 
-    else {
-        console.log (i)
-    }
-}
+// for (let i=0; i <= 45; i ++) {
+//     if (i % 3 === 0){
+//         console.log("fizzbuzz")
+//     } else if (i % 5 === 0) {
+//         console.log ("fizzbuzz")
+//     } 
+//     else {
+//         console.log (i)
+//     }
+// }
 
 
 ////////////////////////////////
 // Wild Wild Life
 ////////////////////////////////
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+// 1. Plantee just had her birthday; change Plantee's array to reflect her being a year older.
+plantee[2] = 5001;
+console.log(plantee)
+
+// 2. Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+wolfy [3] = "Gotham City";
+console.log(wolfy);
+
+// 3. Give D'Art a second hometown by adding "Hawkins"
+dart.push ("Hawkins")
+console.log(dart);
+
+// 4. Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfy array and replace it with "Gameboy".
+wolfy.splice(0, 1, 'Gameboy')
+console.log(wolfy)
 
 
 ////////////////////////////////
