6c6a6b1879f21b702b7da4579de96483563e8ff1
adriancgarcia <adriancgarcia@gmail.com>
2023-05-08 13:21:30 -0500

    Yell at the Ninja Turtles answered

--- answers.js
+++ answers.js
@@ -82,11 +82,51 @@ console.log(wolfy)
 ////////////////////////////////
 // Yell at the Ninja Turtles
 ////////////////////////////////
+// 1. Create an array with the members of the ninja turtles (Donatello, Leonardo, Raphael, Michaelangelo)
+const ninjaTurtles = ['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo']
 
+// 2. Use a for of loop (not a typo - try it out! Try a for of loop) to call toUpperCase() on each of them and print out the result.
+    for (let ninjaTurtle of ninjaTurtles) {
+
+        console.log(ninjaTurtle.toUpperCase());
+    }
 
 ////////////////////////////////
 // Methods, Revisited
 ////////////////////////////////
+// const favMovies = [
+//     'Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+
+//     console.log(favMovies[0])
+
+// 1. use the .sort method Thought question: what did this do to the array? Did it permanently alter it?
+
+
+// 2. Use the method pop
+
+
+// 3. push "Guardians of the Galaxy"
+
+
+// 4. Reverse the array
+
+
+// 5. Use the shift method
+
+
+// 6. unshift - what does it return?
+
+
+// 7. splice "Django Unchained" and add "Avatar" (try finding the index of "Django Unchained", instead of counting it yourself) Thought question: did this permanently alter our array?
+
+
+// 8. slice the last half of the array (challenge yourself and try to programatically determine the middle of the array rather than counting it and hard coding it) - Thought question: did this permanently alter our array?
+
+
+// 9. store the value of your slice in a variable, console.log it - Thought question: what is going on here?
+
+
+// 10. console.log your final results
 
 
 ////////////////////////////////
